ba5eb8ca7e6b6e780759fb85c201ef42e2c75755
Prashant Kumar <prashant.kumar@springfive.com>
2023-03-01 15:25:46 +0530

    test: Add test_download_edgedriver

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
+from edgedriver_autoinstaller.utils import get_edge_version, download_edgedriver
 
 def test_get_edge_version():
-    from edgedriver_autoinstaller.utils import get_edge_version
     version = get_edge_version()
     assert version is None or version.count('.') == 3
+
+def test_download_edgedriver():
+    assert download_edgedriver(True) is not None
+    
